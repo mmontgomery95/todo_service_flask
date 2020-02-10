9928322a6bf2986b69782eb47a6a77f35b93a9d7
Morgan Montgomery <morgan.montgomery95@gmail.com>
2020-02-09 22:38:00 -0600

    create database; create functions to retrieve items and update items status

--- helper.py
+++ helper.py
@@ -1,9 +1,10 @@
 import sqlite3
 
-DB_PATH = './todo.db' # update this path accordingly
-NOTSTARTED = 'Not Started'
-INPROGRESS = 'In Progress'
-COMPLETED = 'Completed'
+DB_PATH = "./todo.db"  # update this path accordingly
+NOTSTARTED = "Not Started"
+INPROGRESS = "In Progress"
+COMPLETED = "Completed"
+
 
 def add_to_list(item):
     try:
@@ -14,11 +15,60 @@ def add_to_list(item):
         c = conn.cursor()
 
         # keep the initial status as Not Started
-        c.execute('insert into items(item, status) values(?,?)', (item, NOTSTARTED))
+        c.execute("insert into items(item, status) values(?,?)", (item, NOTSTARTED))
 
         # commit to save the change
         conn.commit()
-        return{"item": item, "status": NOTSTARTED}
+        return {"item": item, "status": NOTSTARTED}
     except Exception as e:
-        print('Error: ', e)
+        print("Error: ", e)
+        return None
+
+
+def get_all_items():
+    # returns number of items and names of items returned by query
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("select * from items")
+        rows = c.fetchall()  # returns all records; use c.fetchone() for a single item
+        return {"count": len(rows), "items": rows}
+    except Exception as e:
+        print("Error: ", e)
+        return None
+
+
+def get_item(item):
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("select status from items where item=?", (item))
+        status = c.fetchone()[0]
+        return status
+    except Exception as e:
+        print("Error: ", e)
+        return None
+
+
+def update_status(item, status):
+    # check if the passed status is a valid value
+    clean_status = status.lower().strip()
+    if clean_status == "not started":
+        status = NOTSTARTED
+    elif clean_status == "in progress":
+        status = INPROGRESS
+    elif clean_status == "completed":
+        status = COMPLETED
+    else:
+        print("Invalid Status: '%s'" % status)
+        return None
+
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("update items set status=? where item=?", (status, item))
+        conn.commit()
+        return {item: status}
+    except Exception as e:
+        print("Error: ", e)
         return None
--- main.py
+++ main.py
@@ -2,27 +2,96 @@ import helper
 from flask import Flask, request, Response
 import json
 
+print(f"Starting {__name__}")
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
-@app.route('/item/new', methods=['POST'])
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+
+@app.route("/item/new", methods=["POST"])
 def add_item():
     # get item from the POST body
     req_data = request.get_json()
-    item = req_data['item']
+    item = req_data["item"]
 
     # add item to the list
     res_data = helper.add_to_list(item)
 
     # return error if item not added
     if res_data is None:
-        response = Response("{'error': 'Item not added - " + item + "'}", status=400 , mimetype='application/json')
+        response = Response(
+            "{'error': 'Item not added - " + item + "'}",
+            status=500,
+            mimetype="application/json",
+        )
         return response
 
-        #return response
-    response = Response(json.dumps(res_data), mimetype='application/json')
+        # return response
+    response = Response(json.dumps(res_data), mimetype="application/json")
 
-return response
+    return response
+
+
+@app.route("/items/all")  # method defaults to GET
+def get_all_items():
+    # get items from helper
+    res_data = helper.get_all_items()
+
+    # return response
+    response = Response(json.dumps(res_data), mimetype="application/json")
+    return response
+
+
+@app.route("/item/status", methods=["GET"])
+def get_item():
+    # Get parameter from the URL
+    item_name = request.args.get("name")
+
+    # Get items from the helper
+    status = helper.get_item(item_name)
+
+    # Return 404 if item not found
+    if status is None:
+        response = Response(
+            "{'error': 'Item Not Found - %s'}" % item_name,
+            status=404,
+            mimetype="application/json",
+        )
+        return response
+
+    # Return status
+    res_data = {"status": status}
+
+    response = Response(json.dumps(res_data), status=200, mimetype="application/json")
+    return response
+
+
+@app.route("/item/update", methods=["PUT"])
+def update_status():
+    # get the item from POST body
+    req_data = request.get_json()
+    item = req_data["item"]
+    status = req_data["status"]
+
+    # update item in list
+    res_data = helper.update_status(item, status)
+
+    # return error if unable to update status
+    if res_data is None:
+        response = Response(
+            "{'error': 'Error updating item - '" + item + ", " + status + "}",
+            status=400,
+            mimetype="application/json",
+        )
+        return response
+
+    response = Response(json.dumps(res_data), mimetype="application/json")
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
